ddbb814fef29e8d21ac18e682a62d6cc03a19922
alxfed <alxfed@gmail.com>
2019-01-27 11:24:12 -0600

    corrections

---
+++ mean_is_a_verb.py
@@ -0,0 +1,43 @@
+"""
+It is important to understand the etymology of words 'mean', 'meaning',
+'mind'. The underlying word is a verb representing the _process_ of thinking something over, represented in our contemporary English by the verbs 'mean' and 'mind'.
+In this sense the proverbial 'meaning' is just a process of thinking something over, including the process of thinking over the words that were percieved and the result of this effort of thought - understanding (as a noun).
+
+meaning = ['explanation', 'explication']
+"""
+
+"""
+
+noun: meaning
+
+    1. what is meant by a word, text, concept, or action.
+        "the meaning of the word 'supermarket'" 
+    synonyms:	definition, sense, explanation, denotation,
+        connotation, interpretation, elucidation, explication;
+        "the word has several different meanings"
+    2. implied or explicit significance.
+        "he gave me a look full of meaning"
+        synonyms:	significance, sense, signification, import,
+            thrust, drift, gist, implication, tenor, message,
+            essence, substance, purport, intention;
+        "the poem has a hidden meaning"
+            expressiveness, significance, eloquence, implications,
+            intimations, insinuations
+        "his smile was full of meaning"
+    3. important or worthwhile quality; purpose.
+        "this can lead to new meaning in the life of older people"
+        synonyms:	value, validity, worth, consequence, account,
+            use, usefulness, significance, point
+        "my life has no meaning"
+
+adjective: meaning
+
+    1. intended to communicate something that is not directly expressed.
+    "she gave Gabriel a meaning look"
+    synonyms:	meaningful, significant, pointed, eloquent,
+        expressive, pregnant, speaking, telltale, revealing,
+        suggestive
+    "she gave him a meaning look"
+    antonyms:	vacant
+
+"""
