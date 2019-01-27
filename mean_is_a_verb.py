88356789414cd898f8417a83bc936a95d7acef72
alxfed <alxfed@gmail.com>
2019-01-27 12:03:50 -0600

    corrections

--- mean_is_a_verb.py
+++ mean_is_a_verb.py
@@ -41,3 +41,52 @@ adjective: meaning
     antonyms:	vacant
 
 """
+"""
+verb: mean; 3rd person present: means; past tense: meant; past participle: meant; gerund or present participle: meaning
+
+    1. intend to convey, indicate, or refer to (a particular thing or notion); signify.
+    "I don't know what you mean"
+    synonyms:	signify, convey, denote, designate, indicate, connote,
+        show, express, spell out, stand for, represent, symbolize, imply,
+        purport, suggest, allude to, intimate, hint at, insinuate, 
+        drive at, refer to; informal - get at; literary - betoken
+        "the flashing lights mean that the road is blocked"
+        
+        * (of a word) have (something) as its signification in the same
+            language or its equivalent in another language.
+            "its name means “painted rock” in Cherokee"
+            
+        * genuinely intend to convey or express (something).
+            "when she said that before, she meant it"
+        * be of some specified importance to (someone), especially as a
+          source of benefit or object of affection.
+            "animals have always meant more to him than people"
+    2. intend (something) to occur or be the case.
+    "they mean no harm"
+        synonyms:	intend, aim, plan, design, have in mind, have in view,
+            contemplate, think of, purpose, propose, have plans, set out,
+            aspire, desire, want, wish, expect
+        "she didn't mean to break it"
+        * be supposed or intended to do something.
+          "we were meant to go over yesterday"
+            design or destine for a particular purpose.
+          "the jacket was meant for a much larger person"
+          synonyms:	destine, predestine, fate, preordain, ordain;
+          intend, design
+          "he was hit by a bullet meant for a soldier"
+        * have as a motive or excuse in explanation.
+          "what do you mean by leaving me out here in the cold?"
+          synonyms:	matter, have importance, have significance, 
+            be important, be significant; have an input on
+          "this means a lot to me"
+    3. have as a consequence or result.
+      "the proposals are likely to mean another hundred closures"
+      synonyms:	entail, involve, necessitate, lead to, result in, 
+        give rise to, bring about, cause, engender, produce, effect
+      "the closures will mean a rise in unemployment"
+        presage, portend, foretell, augur, promise, foreshadow, 
+        herald, signal, bode; rarebetoken, foretoken, forebode, adumbrate
+        "a red sky in the morning usually means rain"
+      * necessarily or usually entail or involve.
+        "coal stoves mean a lot of smoke"
+"""
